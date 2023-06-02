af7724af3d2d69d34e6b23702f526b823295aab8
Cute <Cute@DESKTOP-QCOA7R0>
2023-06-03 02:05:45 +0400

    Add project files.

---
+++ Lesson1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problem solution 1
+
+            //Console.WriteLine("Hello");
+            //Console.Write("Please write number ");
+            //int number1 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Please write other number ");
+            //int number2 = Convert.ToInt32(Console.ReadLine());
+            //int sum = number1 + number2;
+            //Console.WriteLine("It's a sum your numbers " + sum);
+
+
+            //problem solution with some cases
+
+            //Console.Write("Please write number ");
+            //string str1 = Console.ReadLine();
+            //bool checkResult1 = int.TryParse(str1, out int number1);
+
+            //if (checkResult1 == true)
+            //{
+            //    Console.Write("Please write other number ");
+            //    string str2 = Console.ReadLine();
+            //    bool checkResult2 = int.TryParse(str2, out int number2);
+            //    if (checkResult2 == true)
+            //    {
+            //        int sum = number1 + number2;
+            //        Console.WriteLine("It's your result" + sum);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("It's not number, please write number.");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It's not number, please write number.");
+            //}
+        }
+    }
+}
